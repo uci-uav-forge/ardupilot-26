a2de636d53b10b6d90b2d013a0be7747c97c631c
tylerdoan2004 <tylerdoan2004@gmail.com>
2026-03-11 17:07:53 -0700

    logging in battery message handler

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -1487,7 +1487,15 @@ void GCS_MAVLINK_Copter::handle_message_set_position_target_global_int(const mav
 #endif  // MODE_GUIDED_ENABLED
 
 void GCS_MAVLINK_Copter::handle_message_battery_status(const mavlink_message_t &msg) {
+    mavlink_battery_status_t battery_status;
+    mavlink_msg_battery_status_decode(&msg, &battery_status);
+    
+    gcs().send_text(MAV_SEVERITY_INFO, "Battery status received: temp=%d soc=%d",
+        battery_status.temperature,
+        battery_status.battery_remaining);
+
     gcs().send_to_active_channels(MAVLINK_MSG_ID_BATTERY_STATUS, (const char*)&msg);
+    
 }
 
 void GCS_MAVLINK_Copter::handle_message(const mavlink_message_t &msg)
@@ -1518,6 +1526,7 @@ void GCS_MAVLINK_Copter::handle_message(const mavlink_message_t &msg)
 #endif
 #if AP_BATTERY_ENABLED
     case MAVLINK_MSG_ID_BATTERY_STATUS:
+        gcs().send_text(MAV_SEVERITY_INFO, "Reached battery status case");
         handle_message_battery_status(msg);
         break;
 #endif
